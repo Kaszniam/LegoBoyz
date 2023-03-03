82e1c930b2ee88585337808590c04f2111b036c5
Mati20041 <mati@mati.mati>
2023-03-04 00:27:30 +0100

    init frontend

---
+++ frontend-old/src/views/SegmentListView.tsx
@@ -0,0 +1,7 @@
+import { FunctionComponent } from 'react';
+
+export interface SegmentListViewProps {}
+
+export const SegmentListView: FunctionComponent<SegmentListViewProps> = ({}) => {
+  return <span>Asd</span>;
+};
---
+++ frontend-old/src/views/SegmentView.tsx
@@ -0,0 +1,8 @@
+import {FunctionComponent} from "react";
+
+export interface SegmentViewProps {
+}
+
+export const SegmentView: FunctionComponent<SegmentViewProps> = ({}) => {
+    return (<div>Hello</div>);
+};
