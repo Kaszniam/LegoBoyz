f31d0553c2c4114321f8702837d9c2f3d7ec715c
Mati20041 <mati@mati.mati>
2023-03-04 12:15:23 +0100

    font fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,8 +2,8 @@ import "./App.css";
 import { createTheme, ThemeProvider } from "@mui/material";
 import { Header } from "./components/Header";
 import { Route, Routes } from "react-router-dom";
-import {SegmentListView} from "./views/SegmentListView";
-import {SegmentView} from "./views/SegmentView";
+import { SegmentListView } from "./views/SegmentListView";
+import { SegmentView } from "./views/SegmentView";
 
 const theme = createTheme({});
 
@@ -11,10 +11,10 @@ const App = () => (
   <div className="App">
     <ThemeProvider theme={theme}>
       <Header />
-        <Routes>
-            <Route index element={<SegmentListView/>}/>
-            <Route path="/segments/:id" element={<SegmentView/>}/>
-        </Routes>
+      <Routes>
+        <Route index element={<SegmentListView />} />
+        <Route path="/segments/:id" element={<SegmentView />} />
+      </Routes>
     </ThemeProvider>
   </div>
 );
